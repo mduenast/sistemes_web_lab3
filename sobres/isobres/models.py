a66ae2ddfb063ac58b15523c5c897a70088e31f1
naluem <mdt_ps32468@hotmail.com>
2017-03-07 12:18:14 +0100

    afegit entitat Sobre

--- sobres/isobres/models.py
+++ sobres/isobres/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Sobre(models.Model):
+    amount = models.IntegerField()
+    date = models.DateField()
+    concept = models.TextField(max_length=50)
